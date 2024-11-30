7e13b960ee55aea8f5dba4f3c921b26772dac616
germanica-1 <perezkirvyg@gmail.com>
2024-11-30 18:01:01 +0800

    Working admin login +logout - w/bugs

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
 // server.js
-const express = require('express');
-const mysql = require('mysql2');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const { initializeApp } = require("firebase/app");
+const { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } = require("firebase/auth");
 
 // Initialize Express.js
 const app = express();
 const PORT = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static('public'));
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUSuGNEMUMoPb8bUDa_-0wSKB1XPFsjPU",
+  authDomain: "culiat-web.firebaseapp.com",
+  projectId: "culiat-web",
+  storageBucket: "culiat-web.firebasestorage.app",
+  messagingSenderId: "68213992257",
+  appId: "1:68213992257:web:3d49201eca91b7fb798fc0",
+};
 
-// MySQL connection configuration
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '', // Replace with your MySQL password
-    database: ''  // Replace with your MySQL database name
-});
+initializeApp(firebaseConfig);
 
-// Connect to the MySQL database
-db.connect(err => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err.message);
-    } else {
-        console.log('Connected to MySQL database.');
-    }
-});
+// Express Middlewares
+app.use(express.static("public"));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Default route
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/admin/login', (req, res) => {
-    res.sendFile(__dirname + "/admin/login.html");
+app.get("/admin/login", (req, res) => {
+  res.sendFile(__dirname + "/admin/login.html");
+});
+
+app.post("/api/admin/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Access firebase to login
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, username, password)
+    .then((userCredential) => {
+      // Signed in successfully
+      res.redirect("/");
+    })
+    .catch((error) => {
+      res.redirect("/admin/login");
+    });
+});
+
+app.get("/api/admin/logout", (req, res) => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        res.redirect("/");
+    });
 })
 
-app.get('/admin', (req, res) => {
-    // Authentication
-    authenticated = true;
-
-    if (authenticated)
-        res.sendFile(__dirname + "/admin/admin.html");
-    else
-        res.sendFile(__dirname + '/public/index.html');
-})
+app.get("/admin", (req, res) => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      const uid = user.uid;
+      res.sendFile(__dirname + "/admin/admin.html");
+      // ...
+    } else {
+      // User is signed out
+      res.redirect("/");
+    }
+  });
+});
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
